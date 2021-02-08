7627520244ada47032c2f0e5781f791f5b2aa865
Sam Bentley <sam.bentley@parallo.com>
2021-02-08 17:14:28 +1100

    added comments

--- CollatzSequence.py
+++ CollatzSequence.py
@@ -1,9 +1,12 @@
+# This program lets the user type in an integer that then 
+# keeps calling collatz() on that number until the function returns the value 1
+# For more info: https://en.wikipedia.org/wiki/Collatz_conjecture
+
 def collatz(number):
     if number % 2 == 0:
         return (number // 2)
     else:
         return (3 * number + 1)
-
 
 print("Enter any number:")
 number = input()
