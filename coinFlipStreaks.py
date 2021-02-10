c4d5de2a68e74fd7c1b00d2f7d1ea041c99ad7d2
Sam Bentley <sam.bentley@parallo.com>
2021-02-10 16:03:11 +1100

    initial commit

---
+++ coinFlipStreaks.py
@@ -0,0 +1,18 @@
+import random
+numberOfStreaks = 0
+for experimentNumber in range(10000):
+    # Code that creates a list of 100 'heads' or 'tails' values.
+    results = ''
+    print(results)
+    for experimentNumber in range(100):
+        coinFlip = random.randint(0, 1)
+        if coinFlip == 0:
+            results += 'H'
+        else:
+            results += 'T'
+    numberOfStreaks += results.count('HHHHHH')
+    numberOfStreaks += results.count('TTTTTT')
+
+# Code that checks if there is a streak of 6 heads or tails in a row.
+# Not actually sure if this is mathemetically accurate, but seems close 
+print('Chance of streak: %s%%' % (numberOfStreaks / 10000))
