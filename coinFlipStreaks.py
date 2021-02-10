3d5041d6af861d081d2bf0e0ee9028cfe798533a
Sam Bentley <sam.bentley@parallo.com>
2021-02-10 16:20:18 +1100

    improve logic

--- coinFlipStreaks.py
+++ coinFlipStreaks.py
@@ -3,16 +3,18 @@ numberOfStreaks = 0
 for experimentNumber in range(10000):
     # Code that creates a list of 100 'heads' or 'tails' values.
     results = ''
-    print(results)
     for experimentNumber in range(100):
         coinFlip = random.randint(0, 1)
         if coinFlip == 0:
             results += 'H'
         else:
             results += 'T'
-    numberOfStreaks += results.count('HHHHHH')
-    numberOfStreaks += results.count('TTTTTT')
-
+    if (results.find('HHHHHH')):
+        numberOfStreaks += 1
+    elif (results.find('TTTTTT')):
+        numberOfStreaks += 1
+    #print(results)
 # Code that checks if there is a streak of 6 heads or tails in a row.
 # Not actually sure if this is mathemetically accurate, but seems close 
-print('Chance of streak: %s%%' % (numberOfStreaks / 10000))
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+print(numberOfStreaks)
