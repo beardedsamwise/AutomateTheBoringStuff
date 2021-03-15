c747b06f0b9ea49aa36e9f1dd50003d3121278e6
Sam Bentley <sam.bentley@parallo.com>
2021-03-15 16:18:24 +1100

    remove test code

---
+++ mclip.py
@@ -0,0 +1,16 @@
+text = {'agree': "Yes, I agree. That sounds fine to me.",
+    'busy': "Sorry, can we do this later this week or next week?",
+    'upsell': "Would you like fries with that?"}
+
+import sys, pyperclip
+if len(sys.argv) < 2:
+    print('Usage: python mclip.py [keyphrase] - copy phrase text')
+    sys.exit()
+
+keyphrase = sys.argv[1] # first command line arg is the keyphrase
+
+if keyphrase in text:
+    pyperclip.copy(text[keyphrase])
+    print('Text for ' + keyphrase + ' copied to clipboard.')
+else:
+    print('There is no text for ' + keyphrase)
