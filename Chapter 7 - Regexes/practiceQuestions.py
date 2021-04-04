20491571d2cde3987d5853fdad0d9584a085f50f
Sam Bentley <samjamesbentley@gmail.com>
2021-04-04 14:56:52 +1000

    add comments

--- Chapter 7 - Regexes/practiceQuestions.py
+++ Chapter 7 - Regexes/practiceQuestions.py
@@ -1,6 +1,8 @@
 import re
 
 #Question 21
+# Write a regex that matches the full name of someone whose last name is Watanabe. 
+# You can assume that the first name that comes before it will always be one word that begins with a capital letter.
 
 watanabeRegex = re.compile(r'([A-Z])(\w)+(\s)Watanabe$')
 
