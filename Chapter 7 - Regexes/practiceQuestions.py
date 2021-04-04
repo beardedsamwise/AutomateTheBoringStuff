c0e4c5fab888e2801dc2c538304938ecdce78ac1
Sam Bentley <samjamesbentley@gmail.com>
2021-04-04 14:52:23 +1000

    question 21

--- Chapter 7 - Regexes/practiceQuestions.py
+++ Chapter 7 - Regexes/practiceQuestions.py
@@ -1,12 +1,11 @@
 import re
-#Question 20
-#WIP
-numRegex = re.compile(r'(\d){1,3}(,)?(\d\d\d)?(,)?(\d\d\d)?')
-mo = numRegex.search('444,444')
-print(mo.group(0))
-mo = numRegex.search('444,444,444')
-print(mo.group(0))
-mo = numRegex.search('4444')
-print(mo.group(0))
 
+#Question 21
 
+watanabeRegex = re.compile(r'([A-Z])(\w)+(\s)Watanabe$')
+
+print(watanabeRegex.search('haruto Watanabe')) #must not match
+print(watanabeRegex.search('Mr. Watanabe'))    #must not match
+print(watanabeRegex.search('Watanabe'))        #must not match
+print(watanabeRegex.search('Haruto watanabe')) #must not match
+print(watanabeRegex.search('Haruto Watanabe')) #must match
