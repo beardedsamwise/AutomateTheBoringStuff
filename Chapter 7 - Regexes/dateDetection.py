8e9e7f401df839a7265e4b3ae5a49fda1156420c
Sam Bentley <samjamesbentley@gmail.com>
2021-04-04 16:14:20 +1000

    initial commit

---
+++ Chapter 7 - Regexes/dateDetection.py
@@ -0,0 +1,51 @@
+import re
+
+def isValidDate(date):
+    # create regex to find dates in dd/mm/yyyy format
+    dateRegex = re.compile(r'(\d{2})(/)(\d{2})(/)(\d{4})')
+    mo = dateRegex.search(date)
+
+    # set variables 
+    validDate = False
+    leapYear = False
+    day = int(mo.group(1))
+    month = int(mo.group(3))
+    year = int(mo.group(5))
+    regMonths = [1,3,5,7,8,10,12] # months with 31 days
+    shortMonths = [4,6,9,11] # months with 30 days 
+
+    # check for leap year 
+    if (year % 4 == 0) and (year % 100 != 0):
+        leapYear = True
+    if (year % 400 == 0):
+        leapYear = True
+
+    # check April, June, September, and November which have 30 days
+    if month in shortMonths:
+        if day > 0 and day < 31:
+            validDate = True
+
+    # check February which has 28 days (non leap years)
+    if (month == 2) and (leapYear == False):
+        if day > 0 and day < 29:
+            validDate = True
+        
+    # check February which has 29 days in leap years
+    if (month == 2) and (leapYear == True):
+        if day > 0 and day < 30:
+            validDate = True
+
+    # check all other dates
+    if month in regMonths:
+        if day > 0 and day < 32:
+            validDate = True
+    
+    print(validDate)
+
+isValidDate('22/10/2020') #true
+isValidDate('30/02/2020') #false
+isValidDate('29/02/2020') #true
+isValidDate('29/02/2021') #false
+isValidDate('28/02/2021') #true
+
+
