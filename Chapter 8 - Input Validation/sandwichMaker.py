9fdbf380af04f6e4f4847db4c177c1a89c00e47d
Sam Bentley <samjamesbentley@gmail.com>
2021-08-15 14:09:29 +1000

    add cost calculations

--- Chapter 8 - Input Validation/sandwichMaker.py
+++ Chapter 8 - Input Validation/sandwichMaker.py
@@ -11,13 +11,26 @@ if cheese == "yes":
         ['cheddar', 'Swiss', 'mozzarella'], prompt="Select your cheese type:\n", numbered=True)
 toppings = pyip.inputYesNo(
     prompt="Would you like lettuce, tomato, mayo and mustard?\n")
-numSandwhichs = pyip.inputNum(
+number_sandwich = pyip.inputNum(
     min=1, prompt="How many sandwiches would you like?\n")
 
-# TO DO define prices
+# define prices for sandwich ingredients 
 bread_cost = {'white': 1, 'wheat': 1, 'sourdough': 2}
 protein_cost = {'chicken': 3, 'tofu': 2, 'ham': 3, 'turkey': 3}
 cheese_cost = 2
 toppings_cost = 1
 
-# TO DO calculate sandwhich cost and return to user
+# calculate the cost of the ordered sandwich or sandwiches 
+cost = 0
+cost += (bread_cost.get(bread))
+cost += (protein_cost.get(protein))
+if cheese == "yes":
+    cost += cheese_cost
+if toppings == "yes":
+    cost += toppings_cost
+cost = cost * number_sandwich
+
+if number_sandwich == 1:
+    print("Your sandwich comes to a total of $" + str(cost))
+else:
+    print("You ordered " + str(number_sandwich) + " sandwiches with a total cost of $" + str(cost))
